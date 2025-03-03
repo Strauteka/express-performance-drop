dbd0fc850b7e7937f7cc6c31297dde7bc6907e56
strautins <strautins@mikrotik.com>
2025-03-03 14:00:05 +0200

    adding header on json response

--- src/express_post.js
+++ src/express_post.js
@@ -6,6 +6,12 @@ app.post("/temp/test", (req, res) => {
   res.json(req.body);
 });
 
+app.post("/temp/test/len", (req, res) => {
+  const bodyResponse = JSON.stringify(req.body) + "\n";
+  res.setHeader("Content-Length", Buffer.byteLength(bodyResponse));
+  res.json(bodyResponse);
+});
+
 app.get("/temp/test", (req, res) => {
   res.json({ hello: "world" });
 });
